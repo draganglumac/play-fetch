ddec84b149b07e5946f5c00681ed55de63a39d3c
Dragan Glumac <dragan.glumac@gmail.com>
2016-12-13 12:55:36 +0000

    Prefixing directory paths with _ so that we can have files with the same relative path name as directories.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -3,34 +3,48 @@ require 'digest/sha1'
 require 'fileutils'
 
 def etag_for_file(relative_path)
-  full_path = "#{settings.resources}/#{relative_path}"
+  dir_path = construct_folder_path(relative_path)
+  parts = relative_path.split('/')
+  full_path = "#{dir_path}/#{parts[-1]}"
   contents = File.open(full_path, 'r') { |f| f.read }
   etag(Digest::SHA1.hexdigest contents) unless contents.nil? or contents.empty?
 end
 
 def response_from_file(relative_path)
-  full_path = "#{settings.resources}/#{relative_path}"
+  dir_path = construct_folder_path(relative_path)
+  parts = relative_path.split('/')
+  full_path = "#{dir_path}/#{parts[-1]}"
   send_file(full_path)
 end
 
 def save_request_to_file(request_body, relative_path)
-  parts = relative_path.split('/')
-  if parts.size > 1
-    dir_path = "#{settings.resources}/#{parts[0...-1].join('/')}"
-    FileUtils::mkdir_p(dir_path) # create directories recursively - like mkdir -p
-  end
+  dir_path = construct_folder_path(relative_path)
+  FileUtils::mkdir_p(dir_path) # create directories recursively - like mkdir -p
 
-  full_path = "#{settings.resources}/#{relative_path}"
+  parts = relative_path.split('/')
+  full_path = "#{dir_path}/#{parts[-1]}"
+  
   request_body.rewind
   File.open(full_path, 'wb') { |f| f.write(request_body.read) }
 end
 
 def delete_file(relative_path)
-  path = "#{settings.resources}/#{relative_path}"
+  dir_path = construct_folder_path(relative_path)
+  parts = relative_path.split('/')
+  path = "#{dir_path}/#{parts[-1]}"
   if File::file?(path)
     FileUtils::remove_file(path)
     true
   else
     false
   end  
+end
+
+def construct_folder_path(relative_path)
+  parts = relative_path.split('/')
+  dir_path = settings.resources
+  parts[0..-2].each do |d|
+    dir_path = "#{dir_path}/_#{d}"
+  end
+  dir_path
 end
