015a033179097919fe9c1030c74d8c33a6f2639c
Dragan Glumac <dragan.glumac@gmail.com>
2016-12-14 13:52:38 +0000

    Functional-style processing of the relative path into a folder path.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -3,35 +3,30 @@ require 'digest/sha1'
 require 'fileutils'
 
 def etag_for_file(relative_path)
-  dir_path = construct_folder_path(relative_path)
-  parts = relative_path.split('/')
-  full_path = "#{dir_path}/#{parts[-1]}"
+  dir_path, file = dir_path_and_file(relative_path)
+  full_path = "#{dir_path}/#{file}"
   contents = File.open(full_path, 'r') { |f| f.read }
   etag(Digest::SHA1.hexdigest contents) unless contents.nil? or contents.empty?
 end
 
 def response_from_file(relative_path)
-  dir_path = construct_folder_path(relative_path)
-  parts = relative_path.split('/')
-  full_path = "#{dir_path}/#{parts[-1]}"
+  dir_path, file = dir_path_and_file(relative_path)
+  full_path = "#{dir_path}/#{file}"
   send_file(full_path)
 end
 
 def save_request_to_file(request_body, relative_path)
-  dir_path = construct_folder_path(relative_path)
+  dir_path, file = dir_path_and_file(relative_path)
   FileUtils::mkdir_p(dir_path) # create directories recursively - like mkdir -p
+  full_path = "#{dir_path}/#{file}"
 
-  parts = relative_path.split('/')
-  full_path = "#{dir_path}/#{parts[-1]}"
-  
   request_body.rewind
   File.open(full_path, 'wb') { |f| f.write(request_body.read) }
 end
 
 def delete_file(relative_path)
-  dir_path = construct_folder_path(relative_path)
-  parts = relative_path.split('/')
-  path = "#{dir_path}/#{parts[-1]}"
+  dir_path, file = dir_path_and_file(relative_path)
+  path = "#{dir_path}/#{file}"
   if File::file?(path)
     FileUtils::remove_file(path)
     true
@@ -40,11 +35,8 @@ def delete_file(relative_path)
   end  
 end
 
-def construct_folder_path(relative_path)
+def dir_path_and_file(relative_path)
   parts = relative_path.split('/')
-  dir_path = settings.resources
-  parts[0..-2].each do |d|
-    dir_path = "#{dir_path}/_#{d}"
-  end
-  dir_path
+  dir_path = parts[0..-2].inject(settings.resources) { |path, d| "#{path}/_#{d}" }
+  [dir_path, parts[-1]]
 end
